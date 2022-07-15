64950e8255466875b7a337554cb0ce3ba94bfc2e
SinghShivendra25 <Shivendra2.Singh@lntinfotech.com>
2022-07-15 16:52:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Maths2;
+namespace JenkinApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMaths mymath = new MyMaths();
+            int a = mymath.Add(2, 3);
+            int b = mymath.Multiply(3, 5);
+            Console.WriteLine($"{a}.......{b}");
+        }
+    }
+}
